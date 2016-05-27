30c07dfdc0389dd32862e4fd1c0e69e223ddb90c
Mark Harrison <mark.z.harrison@gmail.com>
2016-05-27 04:18:43 -0700

    New test for loading from gene pool file

--- include/Testing.h
+++ include/Testing.h
@@ -12,6 +12,35 @@
 
 #include "Exceptions/Illegal_Move_Exception.h"
 
+bool files_are_identical(const std::string& file_name1, const std::string& file_name2)
+{
+    std::ifstream file1(file_name1);
+    std::ifstream file2(file_name2);
+    int line_count = 0;
+
+    while(true)
+    {
+        std::string line1, line2;
+        std::getline(file1, line1);
+        std::getline(file2, line2);
+        ++line_count;
+
+        if(line1 != line2)
+        {
+            std::cerr << "Mismatch at line " << line_count << ":\n";
+            std::cerr << line1 << " != " << line2 << "\n";
+            return false;
+        }
+
+        if( ! file1 && ! file2)
+        {
+            break;
+        }
+    }
+
+    return true;
+}
+
 void run_tests()
 {
     // Castling with attacking piece
@@ -89,32 +118,27 @@ void run_tests()
     auto ai2 = Genetic_AI(file_name1);
     ai2.print_genome(file_name2);
 
-    std::ifstream file1(file_name1);
-    std::ifstream file2(file_name2);
-    int line_count = 0;
-
-    while(true)
+    if( ! files_are_identical(file_name1, file_name2))
     {
-        std::string line1, line2;
-        std::getline(file1, line1);
-        std::getline(file2, line2);
-        ++line_count;
-
-        if(line1 != line2)
-        {
-            std::cerr << "Mismatch at line " << line_count << ":\n";
-            std::cerr << line1 << " != " << line2 << "\n";
-            throw Generic_Exception("Test failed!");
-        }
-
-        if( ! file1 && ! file2)
-        {
-            break;
-        }
+        throw Generic_Exception("Test failed!");
     }
-
     remove(file_name1);
     remove(file_name2);
+
+
+    // Load file from gene pool
+    auto gene_pool_file_name = "gene_pool_testing.txt";
+    auto gene_pool_expected_file_name = "gene_pool_42.txt";
+    auto gene_pool_result_file_name = "gene_pool_result.txt";
+    int id = 42;
+    auto gai42 = Genetic_AI(gene_pool_file_name, id);
+    remove(gene_pool_result_file_name);
+    gai42.print_genome(gene_pool_result_file_name);
+
+    if( ! files_are_identical(gene_pool_result_file_name, gene_pool_expected_file_name))
+    {
+        throw Generic_Exception("Test failed!");
+    }
     std::cout << "All tests passed." << std::endl;
 }
 
