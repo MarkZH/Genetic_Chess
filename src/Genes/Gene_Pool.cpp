4dba44e5a050411760f9c370ad09484d31b762b1
Mark Harrison <mark.z.harrison@gmail.com>
2016-07-30 21:53:21 -0700

    Fix spelling

--- src/Genes/Gene_Pool.cpp
+++ src/Genes/Gene_Pool.cpp
@@ -182,7 +182,7 @@ void gene_pool(const std::string& config_file = "")
 
             results.emplace_back(std::async(play_game, white, black, game_time, 0, game_record_file));
 
-            // Limit the number of simultaneous games by waiting for earlier games to finsih
+            // Limit the number of simultaneous games by waiting for earlier games to finish
             // before starting a new one.
             if(results.size() >= maximum_simultaneous_games)
             {
