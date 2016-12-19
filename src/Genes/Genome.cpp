b79c0453ff5184f35c269620c89d623df102781f
Mark Harrison <mark.z.harrison@gmail.com>
2016-12-19 03:40:20 -0800

    No need to check if regulatory genes have valid indices

--- src/Genes/Genome.cpp
+++ src/Genes/Genome.cpp
@@ -34,16 +34,10 @@ Genome::Genome() :
     branch_pruning_gene_index = genome.size() - 1;
 
     // Normal genes
-    if(piece_strength_gene_index < genome.size())
-    {
-        genome.emplace_back(new Total_Force_Gene(std::static_pointer_cast<Piece_Strength_Gene>(genome[piece_strength_gene_index])));
-    }
+    genome.emplace_back(new Total_Force_Gene(std::static_pointer_cast<Piece_Strength_Gene>(genome[piece_strength_gene_index])));
     genome.emplace_back(new Freedom_To_Move_Gene);
     genome.emplace_back(new Pawn_Advancement_Gene);
-    if(piece_strength_gene_index < genome.size())
-    {
-        genome.emplace_back(new Opponent_Pieces_Targeted_Gene(std::static_pointer_cast<Piece_Strength_Gene>(genome[piece_strength_gene_index])));
-    }
+    genome.emplace_back(new Opponent_Pieces_Targeted_Gene(std::static_pointer_cast<Piece_Strength_Gene>(genome[piece_strength_gene_index])));
     genome.emplace_back(new Sphere_of_Influence_Gene);
     genome.emplace_back(new King_Confinement_Gene);
     genome.emplace_back(new King_Protection_Gene);
