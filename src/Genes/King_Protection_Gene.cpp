b5530c24cde667c26f78d68627ff6266c1c57670
Mark Harrison <mark.z.harrison@gmail.com>
2017-01-11 23:51:40 -0800

    Simpler King Protection Gene
    Instead of counting the unguarded squares from which the other side can attack the king (which takes too long and is quickly deactivated (a guess about why it gets deactivated)), just count the empty squares from which the king can be attacked. This relates to how guarded the king is.

--- src/Genes/King_Protection_Gene.cpp
+++ src/Genes/King_Protection_Gene.cpp
@@ -9,16 +9,8 @@ King_Protection_Gene::~King_Protection_Gene()
 
 double King_Protection_Gene::score_board(const Board& board, Color perspective) const
 {
-    // Count the number of unguarded squares from which a piece could attack the king.
-    //
-    // Start at the king, then go by rows, columns, and diagonals. The last square to include
-    // is either the edge of the board or a square containing a piece of either color. The
-    // latter condition holds because that piece blocks squares beyond it. Finally, check knight
-    // moves.
-    //
-    // A square counts as guarded if it is able to be attacked by a friendly piece.
-    //
-    // All squares count equally.
+    // Count the number of empty squares from which a piece could attack the king. This
+    // is a measure of the exposure of the king
 
     auto temp = board;
     temp.set_turn(perspective);
@@ -41,19 +33,19 @@ double King_Protection_Gene::score_board(const Board& board, Color perspective) 
             {
                 char file = king_square.file + steps*file_step;
                 int  rank = king_square.rank + steps*rank_step;
+
                 if(! temp.inside_board(file, rank))
                 {
                     break;
                 }
 
-                if( ! temp.square_attacked_by(file, rank, perspective))
-                {
-                       ++square_count;
-                }
-
                 if(temp.piece_on_square(file, rank))
                 {
                     break;
+                }
+                else
+                {
+                    ++square_count;
                 }
             }
         }
@@ -62,7 +54,7 @@ double King_Protection_Gene::score_board(const Board& board, Color perspective) 
     // Knight moves
     for(auto file_step : {1, 2})
     {
-        auto rank_step  = 3 - file_step; // (1,2) or (2,1)
+        auto rank_step  = 3 - file_step; // (1,2) or (2,1) move
 
         for(auto file_direction : {-1, 1})
         {
@@ -76,7 +68,7 @@ double King_Protection_Gene::score_board(const Board& board, Color perspective) 
                     continue;
                 }
 
-                if( ! temp.square_attacked_by(file, rank, perspective))
+                if( ! temp.piece_on_square(file, rank))
                 {
                     ++square_count;
                 }
@@ -84,9 +76,9 @@ double King_Protection_Gene::score_board(const Board& board, Color perspective) 
         }
     }
 
-    const int max_square_count =  8      // knight
-                                + 7 + 7  // rooks/queen
-                                + 7 + 6; // bishop/queen/pawn
+    const int max_square_count =  8      // knight attack
+                                + 7 + 7  // rooks/queen row/column attack
+                                + 7 + 6; // bishop/queen/pawn attack
     return double(max_square_count - square_count)/max_square_count; // return score [0, 1]
 }
 
