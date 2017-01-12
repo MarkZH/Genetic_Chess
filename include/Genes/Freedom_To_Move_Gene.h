8dd185b184d006b2e646eb4beeaffa1f9d9d930a
Mark Harrison <mark.z.harrison@gmail.com>
2017-01-12 00:43:50 -0800

    Remove unneeded method override declaration from Freedom to Move

--- include/Genes/Freedom_To_Move_Gene.h
+++ include/Genes/Freedom_To_Move_Gene.h
@@ -10,7 +10,6 @@ class Freedom_To_Move_Gene : public Gene
 {
     public:
         Freedom_To_Move_Gene();
-        explicit Freedom_To_Move_Gene(std::ifstream& ifs);
         ~Freedom_To_Move_Gene() override;
 
         Freedom_To_Move_Gene* duplicate() const override;
