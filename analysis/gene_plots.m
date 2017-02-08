a2661a37e8c1bf879aa7501563d37d3fc4a3d724
Mark Harrison <mark.z.harrison@gmail.com>
2017-02-08 03:28:05 -0800

    New plot: Piece Strength Gene activity
    This new plot shows what percentage of AIs are actually using the Piece
    Strength Gene. The gene being in use is defined as when the Piece
    Strength Gene is active and at least one other gene that reads it
    (e.g., the Total Force Gene) is also active.
    
    The latest gene pool runs don't seem to optimize the piece strength gene
    as they did prior to the addition of alpha-beta pruning and the
    Board::king_in_check() simplification. In the latest run, only about 30
    percent of AIs have an active Piece Strength Gene. The top gene is now
    the Sphere of Influence Gene.
    
    My current hypothesis is that the Sphere of Influence Gene still causes
    the AIs to favor queens, rooks, bishops, and knights in that order due
    to how they can cover the board with their movement. Further evidence:
    the queen is still the most popular promotion piece by far. Now that the
    Sphere of Influence Gene has a faster calculation due to the faster
    Board::king_in_check(), it is favored as more informative (combines
    piece strength and position) than the Total Force Gene.

--- analysis/gene_plots.m
+++ analysis/gene_plots.m
@@ -45,6 +45,12 @@ title('Gene Scalar Evolution', 'FontSize', 22);
 
 piece_scalar_plots = [false, false];
 
+total_force_index = 0;
+total_force_prefix = "Total Force Gene";
+opponent_pieces_targeted_index = 0;
+opponent_pieces_prefix = "Opponent Pieces Targeted Gene";
+piece_strength_index = 0;
+
 for yi = 2 : length(data.colheaders) - 2
   this_data = data.data(:, yi);
   name_list = data.colheaders(yi);
@@ -85,11 +91,20 @@ for yi = 2 : length(data.colheaders) - 2
   if name(1:length(piece_strength_prefix)) == piece_strength_prefix
     plot_figure = piece_strength_figure;
     piece_scalar_index = 1;
+    piece_strength_index = yi;
   end
   
   if name(length(name) - length(scalar_suffix) + 1:end) == scalar_suffix
     plot_figure = scalar_figure;
     piece_scalar_index = 2;
+  end
+
+  if strncmp(name, total_force_prefix, length(total_force_prefix))
+    total_force_index = yi;
+  end
+
+  if strncmp(name, opponent_pieces_prefix, length(opponent_pieces_prefix))
+    opponent_pieces_targeted_index = yi;
   end
 
   conv_window = 100;
@@ -131,4 +146,32 @@ if piece_scalar_plots(2)
   set(leg, 'location', 'southoutside');
   legend left;
   print([gene_pool_filename '_gene_scalars.png']);
+end
+
+if total_force_index > 0 && opponent_pieces_targeted_index > 0 && piece_strength_index > 0
+  figure('Position', [0, 0, 1200, 1000]);
+  hold all;
+  total_force_active = isfinite(data.data(:, total_force_index));
+  opponent_targeted_active = isfinite(data.data(:, opponent_pieces_targeted_index));
+  piece_strength_active = isfinite(data.data(:, piece_strength_index));
+  activity = (total_force_active | opponent_targeted_active) & piece_strength_active;
+  smooth_activity = conv(activity, ones(conv_window, 1), 'valid')/conv_window;
+  plot(100*smooth_activity, 'LineWidth', 3, 'displayname', 'Piece Strength Gene active');
+  title('Piece Strength Gene in use', 'FontSize', 22);
+  xlabel('Approximate ID', 'FontSize', 18);
+  ylabel('Percentage with activity', 'FontSize', 18);
+  ylim([0 100]);
+  set(gca, 'FontSize', 14);
+
+  total_force_gene_functional = total_force_active & piece_strength_active;
+  smooth_force = conv(total_force_gene_functional, ones(conv_window, 1), 'valid')/conv_window;
+  plot(100*smooth_force, 'LineWidth', 3, 'displayname', 'Total Force Gene functional');
+
+  opponent_gene_functional = opponent_targeted_active & piece_strength_active;
+  smooth_opponent = conv(opponent_gene_functional, ones(conv_window, 1), 'valid')/conv_window;
+  plot(100*smooth_opponent, 'LineWidth', 3, 'displayname', 'Opponent Pieces Targeted functional');
+
+  leg = legend('show');
+  set(leg, 'FontSize', 12);
+  print([gene_pool_filename '_piece_strength_gene_active.png']);
 end
