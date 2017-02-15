5c60dbb2ce286ebfe63e2edb796b45e2f0198c61
Mark Harrison <mark.z.harrison@gmail.com>
2017-02-15 02:58:17 -0800

    Speed up pawn promotion counter
    delete_comments.sh is a slow function, so filter lines first.

--- analysis/promotions.sh
+++ analysis/promotions.sh
@@ -3,6 +3,5 @@
 # Count how many times each type of piece is picked for a pawn promotion
 
 bindir="$(dirname "$0")"
-#                                     | put each move on own line | look for promotions | get promoted piece alone | sort and count
-./"$bindir"/delete_comments.sh < "$1" | tr -s '[:space:]' '[\n*]' | grep =              | cut -d= -f2 | tr -d +\#  | sort | uniq -c
+grep = "$1" | ./"$bindir"/delete_comments.sh | tr -s '[:space:]' '[\n*]' | grep = | cut -d= -f2 | tr -d +\#  | sort | uniq -c
 
