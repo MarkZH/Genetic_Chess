8964e0ca3b1bde81092061bf038c2d6b7b5bad57
Mark Harrison <mark.z.harrison@gmail.com>
2017-05-03 07:42:27 -0700

    Simplify parsing of gene pool file

--- analysis/gene_pool_analyze.py
+++ analysis/gene_pool_analyze.py
@@ -2,83 +2,70 @@
 
 import sys
 
-def main(argv):
-    current_gene = ''
-    header_line = []
-    ids_at_end = []
+def main(gene_pool_file_name):
     still_alive = dict()
     pool = dict()
-    header_written = False
-    largest_pool_id = -1
-    with open(argv[1] + '_parsed.txt', 'w') as w:
-        with open(argv[1]) as f:
-            for line in f:
-                line = line.strip()
-                if line == 'END' and not header_written:
-                    w.write(','.join(header_line + ['Still Alive', 'Original Pool']) + '\n')
-                    header_written = True
+    with open(gene_pool_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if ':' in line:
+                parameter, value = line.split(':', 1)
+                if parameter == 'ID':
+                    header_line = [parameter]
+                elif parameter == 'Name':
+                    current_gene = value.strip()
+                elif parameter == 'Still Alive':
+                    pool_id, ids = value.split(' : ')
+                    pool_id = pool_id.strip()
+                    ids = ids.split()
+                    still_alive[pool_id] = ids
+                    for ident in ids:
+                        pool[ident] = pool_id
+                else:
+                    header_line.append(current_gene + ' - ' + parameter)
 
-                if ':' in line:
-                    parameter, value = line.split(':', 1)
-                    if parameter == 'ID':
-                        if not header_written:
-                            header_line.append(parameter)
-                        ids_at_end.append(value.strip())
-                    elif parameter == 'Name':
-                        current_gene = value.strip()
-                    elif parameter == 'Still Alive':
-                        ids_at_end = []
-                        pool_id, ids = value.split(' : ')
-                        pool_id = pool_id.strip()
-                        if int(pool_id) > largest_pool_id:
-                            largest_pool_id = int(pool_id)
-                        last_pool_id = pool_id
-                        ids = ids.split()
-                        still_alive[pool_id] = ids
-                        for ident in ids:
-                            pool[ident] = pool_id
-                    else:
-                        header_line.append(current_gene + ' - ' + parameter)
-
-
-        data_line = []
-        still_alive_ids = []
-        for ids in still_alive.values():
-            still_alive_ids += ids
-
-        with open(argv[1]) as f:
-            parameter_count = 0
+        output_file_name = gene_pool_file_name + '_parsed.txt'
+        with open(gene_pool_file_name) as f, open(output_file_name, 'w') as w:
+            data_line = []
+            current_gene = ''
+            w.write(','.join(header_line + ['Still Alive', 'Original Pool']) + '\n')
             for line in f:
                 line = line.strip()
                 if line == 'END':
-                    is_still_alive = str(int(data_line[0] in still_alive_ids))
-                    data_line.append(is_still_alive)
-
                     try:
-                        pool_id = pool[data_line[0]]
-                        data_line.append(pool_id)
+                        ident = data_line[0]
+                        in_pool = pool[ident]
+                        data_line.append(str(int(ident in still_alive[in_pool])))
+                        data_line.append(in_pool)
                         w.write(','.join(data_line) + '\n')
                     except KeyError:
-                        # passed last Still Alive or somehow Pool ID wasn't recorded
+                        # passed last Still Alive or somehow
+                        # Pool ID wasn't recorded
                         pass
 
-                    parameter_count = 0
+                    current_gene = ''
                     data_line = []
-                    continue
 
-
-
-                if ':' in line:
+                elif ':' in line:
                     parameter, value = line.split(':', 1)
-                    if parameter not in ['Name', 'Still Alive']:
-                        data_line.append(value.strip())
-                        parameter_count += 1
+                    value = value.strip()
+                    if parameter == 'Name':
+                        current_gene = value
+                    elif parameter == 'Still Alive':
+                        pass
                     else:
-                        parameter_count = 0
-                elif line.strip() == 'INACTIVE':
-                    for index in range(-parameter_count, 0):
-                        data_line[index] = 'nan'
+                        if current_gene:
+                            title = current_gene + ' - ' + parameter
+                        else:
+                            title = parameter
+                        while not header_line[len(data_line)] == title:
+                            data_line.append('0')
+                        data_line.append(value)
+                elif line == 'INACTIVE':
+                    for index in range(len(header_line)):
+                        if header_line[index].startswith(current_gene):
+                            data_line[index] = 'nan'
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1])
