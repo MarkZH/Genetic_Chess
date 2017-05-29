116f314a7f39220ae69df7d8b1ffd10940e7ada1
Mark Harrison <mark.z.harrison@gmail.com>
2017-05-29 14:51:03 -0700

    Catch error when creating plots

--- analysis/run_all_plots.sh
+++ analysis/run_all_plots.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
 pool_file="$1"
+
+if [[ -z "$pool_file" ]]
+then
+    echo "Specify a gene pool file to analyze."
+    exit
+fi
+
 game_file="${pool_file}_games.txt"
 opening_file="${game_file}_opening_list.txt"
 octave analysis/gene_plots.m "$pool_file" &
