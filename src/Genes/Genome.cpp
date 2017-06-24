e960bcea2ec1f94c15ca1535e1ed66f645821809
Mark Harrison <mark.z.harrison@gmail.com>
2017-06-23 18:07:57 -0700

    Reduce line length and duplication in Genetic_AI ctor

--- src/Genes/Genome.cpp
+++ src/Genes/Genome.cpp
@@ -33,10 +33,12 @@ Genome::Genome()
     look_ahead_gene_index = genome.size() - 1;
 
     // Normal genes
-    genome.emplace_back(std::make_unique<Total_Force_Gene>(static_cast<Piece_Strength_Gene*>(genome[piece_strength_gene_index].get())));
+    auto psg = static_cast<Piece_Strength_Gene*>(genome[piece_strength_gene_index].get());
+
+    genome.emplace_back(std::make_unique<Total_Force_Gene>(psg));
     genome.emplace_back(std::make_unique<Freedom_To_Move_Gene>());
     genome.emplace_back(std::make_unique<Pawn_Advancement_Gene>());
-    genome.emplace_back(std::make_unique<Opponent_Pieces_Targeted_Gene>(static_cast<Piece_Strength_Gene*>(genome[piece_strength_gene_index].get())));
+    genome.emplace_back(std::make_unique<Opponent_Pieces_Targeted_Gene>(psg));
     genome.emplace_back(std::make_unique<Sphere_of_Influence_Gene>());
     genome.emplace_back(std::make_unique<King_Confinement_Gene>());
     genome.emplace_back(std::make_unique<King_Protection_Gene>());
