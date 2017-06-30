aeaed07ea3d37679649aa959a32e33c267e3408b
Mark Harrison <mark.z.harrison@gmail.com>
2017-06-30 06:26:23 -0700

    Move code that returns constant results out of loop in Castling Gene

--- src/Genes/Castling_Possible_Gene.cpp
+++ src/Genes/Castling_Possible_Gene.cpp
@@ -45,37 +45,35 @@ double Castling_Possible_Gene::score_board(const Board& board, Color perspective
     const auto& game_record = board.get_game_record();
     for(size_t i = (perspective == WHITE ? 0 : 1); i < game_record.size(); i += 2)
     {
+        // If already castled, return full score.
         if(game_record[i] == kingside_move || game_record[i] == queenside_move)
         {
             return 1.0;
         }
 
+        // If king moved before castling, return 0.
         if(String::starts_with(game_record[i], starting_king_square))
         {
-            break;
+            return 0.0;
         }
     }
 
     auto score = 0.0;
 
-    if(board.piece_has_moved(king_file, base_rank)) // king moved before castling
-    {
-        return score;
-    }
-
-    // Add score for clearing pieces between king and rook
     for(auto rook_file : {'a', 'h'})
     {
         if( ! board.piece_has_moved(rook_file, base_rank))
         {
+            auto preference = (rook_file == 'h' ? kingside_preference : 1.0 - kingside_preference);
+            int files_to_clear = std::abs(rook_file - king_file) - 1;
+
+            // Add score for clearing pieces between king and rook
             for(char file = std::min(king_file, rook_file) + 1;
                 file < std::max(king_file, rook_file);
                 ++file)
             {
                 if( ! board.view_piece_on_square(file, base_rank))
                 {
-                    auto preference = (rook_file == 'h' ? kingside_preference : 1.0 - kingside_preference);
-                    int files_to_clear = std::abs(rook_file - king_file) - 1;
                     score += preference/(files_to_clear + 1);
                 }
             }
