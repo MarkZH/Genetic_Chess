d414a9e4059ef1317ddb18435706c04c0ff398a8
Mark Harrison <mark.z.harrison@gmail.com>
2017-07-09 20:59:20 -0700

    Start with zero speculative look ahead

--- src/Genes/Look_Ahead_Gene.cpp
+++ src/Genes/Look_Ahead_Gene.cpp
@@ -12,7 +12,7 @@
 
 Look_Ahead_Gene::Look_Ahead_Gene() :
     mean_game_length(50),
-    game_length_uncertainty(0.5),
+    game_length_uncertainty(0.0),
     speculation_constant(0.5)
 {
     recalculate_exponent();
