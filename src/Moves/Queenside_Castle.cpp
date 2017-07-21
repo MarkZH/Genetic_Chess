93a720767bd1f99424d6ecd129fa8d1722a9230a
Mark Harrison <mark.z.harrison@gmail.com>
2017-07-21 03:28:48 -0700

    Put fast legality checks first

--- src/Moves/Queenside_Castle.cpp
+++ src/Moves/Queenside_Castle.cpp
@@ -12,9 +12,9 @@ bool Queenside_Castle::move_specific_legal(const Board& board, char file_start, 
 {
     return     ! board.piece_has_moved(file_start, rank_start)
             && ! board.piece_has_moved('a', rank_start)
+            && ! board.view_piece_on_square('b', rank_start)
             && ! board.king_is_in_check(board.whose_turn())
-            && board.safe_for_king('d', rank_start, board.whose_turn())
-            && ! board.view_piece_on_square('b', rank_start);
+            && board.safe_for_king('d', rank_start, board.whose_turn());
 }
 
 bool Queenside_Castle::can_capture() const
