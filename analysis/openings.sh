ff19c8a9182dba28470b3e020b952dd5a5f3a438
Mark Harrison <mark.z.harrison@gmail.com>
2017-12-01 00:37:30 -0800

    Generalize opening counter script
    openings.sh now takes an argument to specify the number of moves to look
    at when looking at the popularity of opening moves.
    
    Also, fix inaccurate description comment.

--- analysis/openings.sh
+++ analysis/openings.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Count how many times each type of piece is picked for a pawn promotion
+# Determine most often used openings
+
+game_file="$1"
+moves=${2:-1} # number of starting moves (default 1)
+paste_dashes=
+for n in $(seq 1 $moves)
+do
+    paste_dashes=$paste_dashes'- '
+done
 
 bindir="$(dirname "$0")"
-#  First moves   |                                | Save list to file            | sort and count unique lines
-#                |                                | for other uses               | and sort by popularity
-grep '^1\.' "$1" | ./"$bindir"/delete_comments.sh | tee "${1}_opening_list.txt" | sort | uniq -c | sort -n
+#  First n moves                |                                | Delete "--"   | Merge two moves           | Save list to file           | sort and count unique lines
+#                               |                                | between games | to one line               | for other uses              | and sort by popularity
+grep -A$((moves-1)) '^1\.' "$1" | ./"$bindir"/delete_comments.sh | grep -v -- -- | paste -d' ' $paste_dashes | tee "${1}_opening_list.txt" | sort | uniq -c | sort -n
