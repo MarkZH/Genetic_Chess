4c24401754e67823540ddf046bdd5b6598ed9089
Mark Harrison <mark.z.harrison@gmail.com>
2017-12-02 12:29:16 -0800

    Make opening moves an argument in run_all_plots

--- analysis/run_all_plots.sh
+++ analysis/run_all_plots.sh
@@ -3,6 +3,7 @@
 pool_file="$1"
 id_marks_file="$2"
 game_marks_file="$3"
+opening_moves="$4"
 
 if [[ -z "$pool_file" ]]
 then
@@ -14,7 +15,7 @@ game_file="${pool_file}_games.txt"
 opening_file="${game_file}_opening_list.txt"
 octave analysis/gene_plots.m "$pool_file" "$id_marks_file" &
 octave analysis/win_lose_draw_plotting.m "$game_file" "$game_marks_file" &
-./analysis/openings.sh "$game_file" 2 && octave analysis/opening_plotting.m "$opening_file" "$game_marks_file" &
+./analysis/openings.sh "$game_file" "$opening_moves" && octave analysis/opening_plotting.m "$opening_file" "$game_marks_file" &
 wait
 ./analysis/promotions.sh "$game_file"
 ./analysis/castling.sh "$game_file"
