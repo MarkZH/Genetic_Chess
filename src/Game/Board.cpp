346ab2c39822d640220e12f7d0971247aea377b8
Mark Harrison <mark.z.harrison@gmail.com>
2017-12-29 00:33:22 -0800

    Correction to previous commit
    i.e., ddc8ab23aa122d911cb0cdb3cd5904f9402a2110

--- src/Game/Board.cpp
+++ src/Game/Board.cpp
@@ -964,10 +964,10 @@ bool Board::king_is_in_check_after_move(const Move& move) const
         }
 
         // Non-pinned piece moves to block check
-        if(auto blocking_square = piece_is_pinned(move.end_file(), move.end_rank()))
+        if(auto pinning_square = piece_is_pinned(move.end_file(), move.end_rank()))
         {
             // Make sure piece being blocked is acutally doing the checking and the blocking piece can move
-            return (blocking_square == checking_squares.front()) && piece_is_pinned(move.start_file(), move.start_rank());
+            return (pinning_square != checking_squares.front()) || piece_is_pinned(move.start_file(), move.start_rank());
         }
 
         // Nothing is done about check
