6fffe77cfd91e0b16173d11ae8e993671e0d8ea2
Mark Harrison <mark.z.harrison@gmail.com>
2018-06-04 04:10:53 -0700

    Genetic AI file spacing

--- src/Genes/Genome.cpp
+++ src/Genes/Genome.cpp
@@ -198,8 +198,7 @@ void Genome::print(std::ostream& os) const
     {
         gene->print(os);
     }
-    os << "\n";
-}
+S}
 
 double Genome::time_to_examine(const Board& board, const Clock& clock) const
 {
