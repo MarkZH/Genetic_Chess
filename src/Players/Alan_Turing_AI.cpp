e4f80528a0f70e5077009171250e4530cc019abd
Mark Harrison <mark.z.harrison@gmail.com>
2018-07-25 15:14:57 -0700

    New branch for Alan Turing AI

---
+++ src/Players/Alan_Turing_AI.cpp
@@ -0,0 +1,17 @@
+#include "Players/Alan_Turing_AI.h"
+
+#include "Players/Player.h"
+
+Alan_Turing_AI::Alan_Turing_AI()
+{
+}
+
+std::string Alan_Turing_AI::name() const
+{
+    return "Turbocomp";
+}
+
+std::string Alan_Turing_AI::author() const
+{
+    return "Alan Turing";
+}
