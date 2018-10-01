35c93cb49a5fddfff5a2a3624cfc11741c513940
Mark Harrison <mark.z.harrison@gmail.com>
2018-10-01 02:39:25 -0700

    Favor pawn moves and captures for random move picking
    Randomly pick moves that move game forward.

--- src/Players/Random_AI.cpp
+++ src/Players/Random_AI.cpp
@@ -1,9 +1,12 @@
 #include "Players/Random_AI.h"
 
 #include <vector>
+#include <string>
+#include <stdexcept>
 
 #include "Players/Player.h"
 #include "Game/Board.h"
+#include "Pieces/Piece.h"
 #include "Moves/Move.h"
 #include "Utility.h"
 
@@ -17,7 +20,42 @@ const Move& Random_AI::choose_move(const Board& board, const Clock&) const
 const Move* Random_AI::choose_random_move(const Board& board) const
 {
     const auto& moves = board.legal_moves();
-    return moves[Random::random_integer(0, moves.size() - 1)];
+    auto weights = std::vector<int>();
+    weights.reserve(moves.size());
+    auto weight_sum = 0;
+    for(const auto& move : moves)
+    {
+        auto next_board = board;
+        if(next_board.submit_move(*move).winner() != NONE)
+        {
+            return move;
+        }
+
+        auto move_score = 1;
+        if(board.piece_on_square(move->start_file(), move->start_rank())->type() == PAWN)
+        {
+            move_score += 2;
+        }
+        if(board.move_captures(*move))
+        {
+            move_score += 4;
+        }
+
+        weights.push_back(move_score);
+        weight_sum += move_score;
+    }
+
+    auto score_pick = Random::random_integer(0, weight_sum);
+    for(size_t i = 0; i < weights.size(); ++i)
+    {
+        score_pick -= weights[i];
+        if(score_pick <= 0)
+        {
+            return moves[i];
+        }
+    }
+
+    throw std::logic_error("Bug in Random move picking algorithm");
 }
 
 std::string Random_AI::name() const
