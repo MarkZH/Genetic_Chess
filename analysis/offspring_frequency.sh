b05ed7066647c1320b418028fef58c0d11213f9e
Mark Harrison <mark.z.harrison@gmail.com>
2018-11-18 23:18:52 -0800

    Create plots from offspring data
    A fit of the child count vs. frequency (i.e., the number of AI that
    created N children) is very close to y = (1/2)^N. This indicates that,
    for the most part, every AI has a 50/50 chance of winning. The
    frequencies for large N tend to be above the fit line. Whether the
    interpretation of these points is noise in small numbers or indicators
    of very advantageous mutations remains to be seen.

--- analysis/offspring_frequency.sh
+++ analysis/offspring_frequency.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 
-grep "\[" "$1" |
-    grep "White\|Black" |
-    cut -d\" -f2 |
-    cut -d' ' -f3 |
-    sort -n |
-    uniq -c |
-    sed "s/^ *//" |
-    cut -d" " -f1 |
-    sort -n |
-    uniq -c |
-    column -t -o\; -O2,1 -N"Frequency,Child count"
+# Every game played results in a child AI, so the number of games
+# played by an AI equals the number of its offspring.
+
+input_file="$1"
+output_file="${input_file}_offspring_counts.txt"
+grep "\[" "$input_file" |           # Find header lines
+    grep "White\|Black" |  # Find players
+    cut -d\" -f2 |         # Cut out name of player
+    cut -d' ' -f3 |        # Cut out ID number
+    sort -n |              # Sort by number, then
+    uniq -c |              # count appearances of ID (equals number of children)
+    sed "s/^ *//" |        # Delete leading whitespace
+    cut -d" " -f1 |        # Get list of number of children by each AI
+    sort -n |              # Sort that list to
+    uniq -c |              # count number of time an AI has sired N children
+    column -t -o\; -O2,1 -N"Frequency,Child count" |
+    tee "$output_file"
+
+octave ./analysis/offspring_plots.m "$output_file"
---
+++ analysis/offspring_plots.m
@@ -0,0 +1,69 @@
+isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+
+filename = 0;
+directory = '';
+if isOctave
+    graphics_toolkit("gnuplot");
+    args = argv();
+    if length(args) > 0
+        filename = args{1};
+    end
+end
+
+if filename == 0
+    [filename, directory, ~] = uigetfile();
+end
+
+if filename == 0
+    return
+end
+raw_data_file_name = fullfile(directory, filename);
+
+data = importdata(raw_data_file_name, ';');
+
+child_count = data.data(:, 1);
+frequency = data.data(:, 2);
+x_axis_name = data.colheaders(1);
+y_axis_name = data.colheaders(2);
+
+figure;
+hold all;
+scatter(child_count, frequency, 'LineWidth', 3);
+xlabel(x_axis_name);
+ylabel(y_axis_name);
+title('Offspring Count');
+
+% Exponential fit assuming y = A*exp(B*x)
+% Uses a linear least-squares fit on x and log(y) while putting more weight on large y values
+% by minimizing D = sum(y.*(log(y) - a - b*x).^2)
+%
+% Weisstein, Eric W. "Least Squares Fitting--Exponential."
+% From MathWorld--A Wolfram Web Resource.
+% http://mathworld.wolfram.com/LeastSquaresFittingExponential.html
+
+x = child_count;
+y = frequency;
+
+a = (sum((x.^2).*y)*sum(y.*log(y)) - sum(x.*y)*sum(x.*y.*log(y)))/(sum(y)*sum((x.^2).*y) - sum(x.*y)^2);
+b = (sum(y)*sum(x.*y.*log(y)) - sum(x.*y)*sum(y.*log(y)))/(sum(y)*sum((x.^2).*y) - sum(x.*y)^2);
+
+B = -b;
+A = exp(a);
+
+x_fit = (min(x) : (max(x) - min(x))/1000 : max(x));
+y_fit = A*exp(b*x_fit);
+
+plot(x_fit, y_fit, 'LineWidth', 3);
+
+fit_text = {'y = Ae^{-Bx}',
+            '',
+            ['A = ' num2str(A)],
+            '',
+            ['B = ' num2str(B) ' = log(' num2str(exp(B)) ')']};
+xl = xlim;
+yl = ylim;
+text(0.6*xl(2), 0.4*yl(2), fit_text, 'FontSize', 14);
+
+print([raw_data_file_name '_offspring_plot.png']);
+set(gca, 'yscale', 'log');
+print([raw_data_file_name '_offspring_plot_log.png']);
