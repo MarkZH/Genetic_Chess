2dc6f46ce0802ff850d57785c99d1a192f80af7c
Mark Harrison <mark.z.harrison@gmail.com>
2018-12-11 20:29:35 -0800

    Fix indentation to only open file once at a time

--- analysis/gene_pool_analyze.py
+++ analysis/gene_pool_analyze.py
@@ -30,46 +30,46 @@ def main(gene_pool_file_name):
                 else:
                     header_line.append(current_gene + ' - ' + parameter)
 
-        # Read gene pool file for data
-        output_file_name = gene_pool_file_name + '_parsed.txt'
-        with open(gene_pool_file_name) as f, open(output_file_name, 'w') as w:
-            data_line = []
-            current_gene = ''
-            w.write(','.join(header_line + ['Still Alive', 'Original Pool']) + '\n')
-            for line in f:
-                line = line.strip()
-                if line == 'END':
-                    try:
-                        ident = data_line[0]
-                        in_pool = pool[ident]
-                        data_line.append(str(int(ident in still_alive[in_pool])))
-                        data_line.append(in_pool)
-                        w.write(','.join(data_line) + '\n')
-                    except KeyError:
-                        # passed last Still Alive or somehow
-                        # Pool ID wasn't recorded
-                        pass
+    # Read gene pool file for data
+    output_file_name = gene_pool_file_name + '_parsed.txt'
+    with open(gene_pool_file_name) as f, open(output_file_name, 'w') as w:
+        data_line = []
+        current_gene = ''
+        w.write(','.join(header_line + ['Still Alive', 'Original Pool']) + '\n')
+        for line in f:
+            line = line.strip()
+            if line == 'END':
+                try:
+                    ident = data_line[0]
+                    in_pool = pool[ident]
+                    data_line.append(str(int(ident in still_alive[in_pool])))
+                    data_line.append(in_pool)
+                    w.write(','.join(data_line) + '\n')
+                except KeyError:
+                    # passed last Still Alive or somehow
+                    # Pool ID wasn't recorded
+                    pass
 
-                    current_gene = ''
-                    data_line = []
+                current_gene = ''
+                data_line = []
 
-                elif ':' in line:
-                    parameter, value = line.split(':', 1)
-                    value = value.strip()
-                    if parameter == 'Name':
-                        current_gene = value
-                    elif parameter == 'Still Alive':
-                        continue
-                    elif parameter == 'Ancestry':
-                        data_line.extend([x.split('->')[1] for x in value.split('/') if x])
+            elif ':' in line:
+                parameter, value = line.split(':', 1)
+                value = value.strip()
+                if parameter == 'Name':
+                    current_gene = value
+                elif parameter == 'Still Alive':
+                    continue
+                elif parameter == 'Ancestry':
+                    data_line.extend([x.split('->')[1] for x in value.split('/') if x])
+                else:
+                    if current_gene:
+                        title = current_gene + ' - ' + parameter
                     else:
-                        if current_gene:
-                            title = current_gene + ' - ' + parameter
-                        else:
-                            title = parameter
-                        while not header_line[len(data_line)] == title:
-                            data_line.append('0')
-                        data_line.append(value)
+                        title = parameter
+                    while not header_line[len(data_line)] == title:
+                        data_line.append('0')
+                    data_line.append(value)
 
 
 if __name__ == '__main__':
