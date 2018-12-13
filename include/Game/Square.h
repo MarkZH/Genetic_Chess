f70329ffde950e1b85dd2a8984f36d87a62ce456
Mark Harrison <mark.z.harrison@gmail.com>
2018-12-12 22:33:33 -0800

    Delete unused function

--- include/Game/Square.h
+++ include/Game/Square.h
@@ -22,6 +22,5 @@ int king_distance(const Square& a, const Square& b);
 
 bool operator==(const Square& a, const Square& b);
 bool operator!=(const Square& a, const Square& b);
-bool operator<(const Square& a, const Square& b);
 
 #endif // SQUARE_H
