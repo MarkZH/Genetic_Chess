aa87d8e323ad62a433c0a4dd1b577209f54a60f9
Mark Harrison <mark.z.harrison@gmail.com>
2019-02-22 05:57:01 -0800

    Fix comment

--- analysis/castling.sh
+++ analysis/castling.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Count how many times each type of piece is picked for a pawn promotion
+# Count how many times a castling move was picked (O-O = kingside, O-O-O = queenside)
 
 bindir="$(dirname "$0")"
 (echo "# Castling" ; grep O "$1" | ./"$bindir"/delete_comments.sh | tr -s '[:space:]' '[\n*]' | grep O | tr -d +\#  | sort | uniq -c)
 echo "out of $(grep -c Round "$1") games"
-
