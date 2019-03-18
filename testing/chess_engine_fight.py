88030d624c02bbb53daea166dc18234c48377304
Mark Harrison <mark.z.harrison@gmail.com>
2019-03-18 01:09:52 -0700

    Simplify chess_engine_fight.py
    No change in behavior.

--- testing/chess_engine_fight.py
+++ testing/chess_engine_fight.py
@@ -8,38 +8,34 @@ if len(sys.argv) < 2:
         print(str(i) + ': ' + sys.argv[i])
     sys.exit(1)
 
-first_engine = './' + sys.argv[-2]
-second_engine = './' + sys.argv[-1]
+generator = './' + sys.argv[-2]
+checker = './' + sys.argv[-1]
 game_file = 'game.pgn'
 count = 0
 
 while True:
-    for generator in [first_engine, second_engine]:
-        try:
-            os.remove(game_file)
-        except OSError:
-            pass
+    try:
+        os.remove(game_file)
+    except OSError:
+        pass
 
-        count += 1
-        print('Game #' + str(count))
+    count += 1
+    print('Game #' + str(count))
 
-        if generator == first_engine:
-            checker = second_engine
-        else:
-            checker = first_engine
+    out = subprocess.run([generator, '-random', '-random'])
+    if not os.path.isfile(game_file):
+        print('Game file not produced: ' + game_file)
+        print('generator = ' + generator)
+        print(out.returncode)
+        print(out.stdout)
+        print(out.stderr)
+        sys.exit()
 
-        out = subprocess.run([generator, '-random', '-random'])
-        if not os.path.isfile(game_file):
-            print('Game file not produced: ' + game_file)
-            print('generator = ' + generator)
-            print(out.returncode)
-            print(out.stdout)
-            print(out.stderr)
-            sys.exit()
+    result = subprocess.run([checker, '-confirm', game_file])
+    if result.returncode != 0:
+        print('Found discrepancy. See ' + game_file)
+        print('generator = ' + generator)
+        print('checker = ' + checker)
+        sys.exit()
 
-        result = subprocess.run([checker, '-confirm', game_file])
-        if result.returncode != 0:
-            print('Found discrepancy. See ' + game_file)
-            print('generator = ' + generator)
-            print('checker = ' + checker)
-            sys.exit()
+    generator, checker = checker, generator
