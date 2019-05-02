4f0d673185e5f3d7e171481e6825f255a6733c21
Mark Harrison <mark.z.harrison@gmail.com>
2019-05-01 23:38:01 -0700

    Offspring plot improvements
    Instead of fitting a curve to the data, plot the null hypothesis, which
    assumes that every Genetic AI has a 50/50 chance of winning. Deviations
    from this plot should show that some AIs really are bette than others.
    Specifically, an excess of AIs with large numbers of offspring should
    mean that substantial chess-playing improvements are being made.
    
    Also, code organization; plus, octave can't put a legend with a scatter
    plot, so use the plot() function with extra options.

--- analysis/offspring_plots.m
+++ analysis/offspring_plots.m
@@ -23,47 +23,32 @@ data = importdata(raw_data_file_name, ';');
 
 child_count = data.data(:, 1);
 frequency = data.data(:, 2);
-x_axis_name = data.colheaders(1);
-y_axis_name = data.colheaders(2);
 
 figure;
 hold all;
-scatter(child_count, frequency, 'LineWidth', 3);
+
+plot(child_count, frequency, '.', 'LineWidth', 3, 'DisplayName', 'Data');
 high_err = sqrt(frequency);
 low_err = high_err;
 low_err(frequency - low_err <= 0) = 0;
 errorbar(child_count, frequency, low_err, high_err, '.');
-xlabel(x_axis_name);
-ylabel(y_axis_name);
-title('Offspring Count');
 
-% Exponential fit assuming y = A*exp(B*x)
-% Uses a linear least-squares fit on x and log(y) while putting more weight on large y values
-% by minimizing D = sum(y.*(log(y) - a - b*x).^2)
-%
-% Weisstein, Eric W. "Least Squares Fitting--Exponential."
-% From MathWorld--A Wolfram Web Resource.
-% http://mathworld.wolfram.com/LeastSquaresFittingExponential.html
+A = sum(child_count.*frequency)/2;
+x_fit = linspace(1, max(child_count), 1000);
+y_fit = A*(1/2).^(x_fit);
 
-x = child_count;
-y = frequency;
+plot(x_fit, y_fit, 'LineWidth', 3, 'DisplayName', 'Null hypothesis');
 
-a = (sum((x.^2).*y)*sum(y.*log(y)) - sum(x.*y)*sum(x.*y.*log(y)))/(sum(y)*sum((x.^2).*y) - sum(x.*y)^2);
-b = (sum(y)*sum(x.*y.*log(y)) - sum(x.*y)*sum(y.*log(y)))/(sum(y)*sum((x.^2).*y) - sum(x.*y)^2);
-
-A = exp(a);
-
-x_fit = (min(x) : (max(x) - min(x))/1000 : max(x));
-y_fit = A*exp(b*x_fit);
-
-plot(x_fit, y_fit, 'LineWidth', 3);
-
-fit_text = {'y = Ae^{bx}',
-            ['A = ' num2str(A)],
-            ['b = ' num2str(b) ' = log(' num2str(exp(b)) ')']};
+fit_text = {'y = A(1/2)^{x}',
+            ['A = ' num2str(A) ' = # games = # offspring']};
 xl = xlim;
 yl = ylim;
 text(0.6*xl(2), 0.4*yl(2), fit_text);
+
+leg = legend('show');
+xlabel(data.colheaders(1));
+ylabel(data.colheaders(2));
+title('Offspring Count');
 
 print([raw_data_file_name '_offspring_plot.png']);
 set(gca, 'yscale', 'log');
