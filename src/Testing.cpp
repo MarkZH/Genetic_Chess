ef9200698ff56189481f73208766ab7e61ea8c73
Mark Harrison <mark.z.harrison@gmail.com>
2019-05-13 19:24:26 -0700

    Make type conversion explicit

--- src/Testing.cpp
+++ src/Testing.cpp
@@ -813,7 +813,7 @@ bool run_perft_tests()
 
     auto time = perft_timer.time_so_far();
     std::cout << "Perft time: " << time << " seconds" << std::endl;
-    std::cout << "Legal moves counted: " << String::format_integer(legal_moves_counted, ",") << std::endl;
+    std::cout << "Legal moves counted: " << String::format_integer(int(legal_moves_counted), ",") << std::endl;
     std::cout << "Move generation rate: " << String::format_integer(int(legal_moves_counted/time), ",") << " moves/second." << std::endl;
     return true;
 }
