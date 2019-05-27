15d42e1d1a88289e2e1129a4c9eebe0f42d60012
Mark Harrison <mark.z.harrison@gmail.com>
2019-05-27 05:29:27 -0700

    Use <algorithm>s to simplify Board hash intialization

--- src/Game/Board.cpp
+++ src/Game/Board.cpp
@@ -36,10 +36,10 @@ namespace
         std::array<std::array<uint64_t, 13>, 64> hash_cache;
         for(auto& square_indexed_row : hash_cache)
         {
-            for(auto& piece_indexed_random_int : square_indexed_row)
-            {
-                piece_indexed_random_int = Random::random_unsigned_int64();
-            }
+            std::transform(square_indexed_row.begin(),
+                           square_indexed_row.end(),
+                           square_indexed_row.begin(),
+                           [](auto) { return Random::random_unsigned_int64(); });
         }
         return hash_cache;
     }();
@@ -49,10 +49,10 @@ namespace
     {
         // One entry for each file
         std::array<uint64_t, 8> en_passant_hash_cache;
-        for(auto& file_indexed_random_int : en_passant_hash_cache)
-        {
-            file_indexed_random_int = Random::random_unsigned_int64();
-        }
+        std::transform(en_passant_hash_cache.begin(),
+                       en_passant_hash_cache.end(),
+                       en_passant_hash_cache.begin(),
+                       [](auto) { return Random::random_unsigned_int64(); });
         return en_passant_hash_cache;
     }();
 
@@ -61,10 +61,10 @@ namespace
     {
         // One entry for each rook
         std::array<uint64_t, 4> castling_hash_cache;
-        for(auto& n : castling_hash_cache)
-        {
-            n = Random::random_unsigned_int64();
-        }
+        std::transform(castling_hash_cache.begin(),
+                       castling_hash_cache.end(),
+                       castling_hash_cache.begin(),
+                       [](auto) { return Random::random_unsigned_int64(); });
         return castling_hash_cache;
     }();
 
