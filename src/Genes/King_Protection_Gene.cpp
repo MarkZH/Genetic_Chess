2d09f40e3d03a30db9fb5273b799e4f4463850d7
Mark Harrison <mark.z.harrison@gmail.com>
2019-06-13 14:12:27 -0700

    Use attack indices to simplify King Protection scoring

--- src/Genes/King_Protection_Gene.cpp
+++ src/Genes/King_Protection_Gene.cpp
@@ -7,63 +7,33 @@
 #include "Game/Board.h"
 #include "Game/Color.h"
 #include "Game/Square.h"
+#include "Moves/Move.h"
 
 double King_Protection_Gene::score_board(const Board& board, Color perspective, size_t) const
 {
-    const auto& king_square = board.find_king(perspective);
-
     auto square_count = 0;
 
-    // Rows, columns, diagonals
-    for(auto file_step : {-1, 0, 1})
+    for(size_t attack_index = 0; attack_index < 16; ++attack_index)
     {
-        for(auto rank_step : {-1, 0, 1})
+        auto step = Move::attack_direction_from_index(attack_index);
+        for (auto square : Square::square_line_from(board.find_king(perspective), step))
         {
-            if(file_step == 0 && rank_step == 0)
+            if (board.piece_on_square(square))
             {
-                continue;
+                break;
+            }
+            else
+            {
+                ++square_count;
             }
 
-            auto step = Square_Difference{file_step, rank_step};
-            for(auto square : Square::square_line_from(king_square, step))
+            if(attack_index >= 8) // knight move
             {
-                if(board.piece_on_square(square))
-                {
-                    break;
-                }
-                else
-                {
-                    ++square_count;
-                }
+                break;
             }
         }
     }
-
-    // Knight moves
-    for(auto file_step : {1, 2})
-    {
-        auto rank_step  = 3 - file_step; // (1,2) or (2,1) move
-
-        for(auto file_direction : {-1, 1})
-        {
-            for(auto rank_direction : {-1, 1})
-            {
-                auto square = king_square + Square_Difference{file_direction*file_step,
-                                                              rank_direction*rank_step};
-
-                if( ! square.inside_board())
-                {
-                    continue;
-                }
-
-                if( ! board.piece_on_square(square))
-                {
-                    ++square_count;
-                }
-            }
-        }
-    }
-
+    
     constexpr int max_square_count =  8      // knight attack
                                     + 7 + 7  // rooks/queen row/column attack
                                     + 7 + 6; // bishop/queen/pawn attack
