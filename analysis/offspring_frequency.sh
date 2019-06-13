c7435ca98ab3254a14991ae74f971ed865a7d007
Mark Harrison <mark.z.harrison@gmail.com>
2019-06-13 14:25:15 -0700

    Align comments

--- analysis/offspring_frequency.sh
+++ analysis/offspring_frequency.sh
@@ -12,15 +12,15 @@ else
 fi
 output_file="${input_file}_offspring_counts.txt"
 grep "\[White \|\[Black " "$input_file" |  # Find players
-    cut -d\" -f2 |         # Cut out name of player
-    cut -d' ' -f3 |        # Cut out ID number
-    sort -n |              # Sort by number, then
-    uniq -c |              # count appearances of ID (equals number of children)
-    sed "s/^ *//" |        # Delete leading whitespace
-    cut -d" " -f1 |        # Get list of number of children by each AI
+    cut -d\" -f2 |            # Cut out name of player
+    cut -d' ' -f3 |           # Cut out ID number
+    sort -n |                 # Sort by number
+    uniq -c |                 # count appearances of ID (equals number of children)
+    sed "s/^ *//" |           # Delete leading whitespace
+    cut -d" " -f1 |           # Get list of number of children by each AI
     awk '{print $1-'"$adj}" | # Subtract 1 if gene pool was cloning
-    sort -n |              # Sort that list to
-    uniq -c |              # count number of time an AI has sired N children
+    sort -n |                 # Sort that list to
+    uniq -c |                 # count number of time an AI has sired N children
     column -t -o\; -O2,1 -N"Frequency,Child count" > "$output_file"
 
 octave ./analysis/offspring_plots.m "$output_file"
