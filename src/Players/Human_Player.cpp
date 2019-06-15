c6d4cf581cb9b8999c2d852fe08a259b8f8d6b4e
Mark Harrison <mark.z.harrison@gmail.com>
2019-06-15 10:54:05 -0700

    Flatten try-catch in Human_Player::choose_move()

--- src/Players/Human_Player.cpp
+++ src/Players/Human_Player.cpp
@@ -53,23 +53,20 @@ const Move& Human_Player::choose_move(const Board& board, const Clock& clock) co
 
         try
         {
-            try
+            return board.create_move(move);
+        }
+        catch(const Promotion_Piece_Needed&)
+        {
+            const std::string choices = "BNRQ";
+            std::string promote = "--";
+            while(promote.size() != 1 || ! String::contains(String::lowercase(choices), String::lowercase(promote)))
             {
-                return board.create_move(move);
+                std::cout << "What should the pawn be promoted to?\n";
+                std::cout << "Choice: [" << choices << "]: ";
+                std::getline(std::cin, promote);
+                promote = String::trim_outer_whitespace(promote);
             }
-            catch(const Promotion_Piece_Needed&)
-            {
-                const std::string choices = "BNRQ";
-                std::string promote = "--";
-                while(promote.size() != 1 || ! String::contains(String::lowercase(choices), String::lowercase(promote)))
-                {
-                    std::cout << "What should the pawn be promoted to?\n";
-                    std::cout << "Choice: [" << choices << "]: ";
-                    std::getline(std::cin, promote);
-                    promote = String::trim_outer_whitespace(promote);
-                }
-                return board.create_move(move + promote);
-            }
+            return board.create_move(move + promote);
         }
         catch(const Illegal_Move& e)
         {
