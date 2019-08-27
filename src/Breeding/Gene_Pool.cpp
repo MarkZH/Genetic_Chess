f91e5474a07189ed6807cb36363aaa7262dbc9d9
Mark Harrison <mark.z.harrison@gmail.com>
2019-08-26 22:47:14 -0700

    Fix shadowed variable

--- src/Breeding/Gene_Pool.cpp
+++ src/Breeding/Gene_Pool.cpp
@@ -382,8 +382,7 @@ void gene_pool(const std::string& config_file)
         }
 
         // widths of columns for stats printout
-        auto max_id = pool.back().id();
-        auto id_digits = int(std::floor(std::log10(max_id) + 1));
+        auto id_digits = int(std::floor(std::log10(pool.back().id()) + 1));
 
         // Write stat headers
         std::cout << '\n' << std::setw(id_digits + 1)  << "ID"
