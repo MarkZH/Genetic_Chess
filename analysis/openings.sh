4e0e983f319cb8b2ab029d3addec2b3f7e6a7995
Mark Harrison <mark.z.harrison@gmail.com>
2020-02-20 15:14:06 -0800

    Remove game ending from openings

--- analysis/openings.sh
+++ analysis/openings.sh
@@ -12,6 +12,6 @@ done
 
 bindir="$(dirname "$0")"
 echo "# Most popular openings:"
-#  First n moves                        |                                | Delete "--"   | Merge two moves           | Save list to file           | sort and count unique lines
-#                                       |                                | between games | to one line               | for other uses              | and sort by popularity
-grep -A$((moves-1)) '^1\.' "$game_file" | ./"$bindir"/delete_comments.sh | grep -v -- -- | paste -d' ' $paste_dashes | tee "${1}_opening_list.txt" | sort | uniq -c | sort -n | tee >(echo $(wc -l) different openings)
+#  First n moves                        |                                | Delete "--"   | Merge two moves           | Take only the    | Save list to file           | sort and count unique lines
+#                                       |                                | between games | to one line               | move text.       | for other uses              | and sort by popularity
+grep -A$((moves-1)) '^1\.' "$game_file" | ./"$bindir"/delete_comments.sh | grep -v -- -- | paste -d' ' $paste_dashes | cut -d' ' -f 1-3 | tee "${1}_opening_list.txt" | sort | uniq -c | sort -n | tee >(echo $(wc -l) different openings)
