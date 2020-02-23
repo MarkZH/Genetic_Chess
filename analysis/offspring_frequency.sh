a9c4bab7b0ad624294e928af8d33e7fdd8b6da7a
Mark Harrison <mark.z.harrison@gmail.com>
2020-02-22 23:04:10 -0800

    Create an offspring plot for last 10% of games
    There's an annoying amount of code duplication here but Octave
    is annoying about what is allowed in the same file.

--- analysis/offspring_frequency.sh
+++ analysis/offspring_frequency.sh
@@ -3,24 +3,9 @@
 # Every game played results in a child AI, so the number of games
 # played by an AI equals the number of its offspring.
 
-input_file="$1"
-if [[ "$2" == "cloning" ]]
-then
-    adj=1
-else
-    adj=0
-fi
-output_file="${input_file}_offspring_counts.txt"
-grep "Genetic" "$input_file" |  # Find players
-    cut -d\" -f2 |            # Cut out name of player
-    cut -d' ' -f3 |           # Cut out ID number
-    sort -n |                 # Sort by number
-    uniq -c |                 # count appearances of ID (equals number of children)
-    sed "s/^ *//" |           # Delete leading whitespace
-    cut -d" " -f1 |           # Get list of number of children by each AI
-    awk '{print $1-'"$adj}" | # Subtract 1 if gene pool was cloning
-    sort -n |                 # Sort that list to
-    uniq -c |                 # count number of time an AI has sired N children
-    column -t -o\; -O2,1 -N"Frequency,Child count" > "$output_file"
+output_file="${1}_offspring_counts.txt"
+grep "Genetic" "$1" |  # Find players
+    cut -d\" -f2 |     # Cut out name of player
+    cut -d' ' -f3 > "$output_file" # Cut out ID number
 
-octave ./analysis/offspring_plots.m "$output_file"
+octave ./analysis/offspring_plots.m "$output_file" "$2"
--- analysis/offspring_plots.m
+++ analysis/offspring_plots.m
@@ -2,11 +2,15 @@ isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 
 filename = 0;
 directory = '';
+adj = 0;
 if isOctave
     graphics_toolkit("gnuplot");
     args = argv();
     if length(args) > 0
         filename = args{1};
+        if strcmp(args{2}, "cloning")
+            adj=1
+        end
     end
 end
 
@@ -19,31 +23,61 @@ if filename == 0
 end
 raw_data_file_name = fullfile(directory, filename);
 
-data = importdata(raw_data_file_name, ';');
+data = importdata(raw_data_file_name);
+data_last_10p = data(floor(0.9*length(data)) : end);
 
-child_count = data.data(:, 1);
-frequency = data.data(:, 2);
+unique_ids = unique(data);
+offspring_counts = zeros(size(unique_ids));
+
+unique_ids_last_10p = unique(data_last_10p);
+offspring_counts_last_10p = zeros(size(unique_ids_last_10p));
+
+for index = 1 : length(unique_ids)
+    id = unique_ids(index);
+    offspring_counts(index) = sum(id == data);
+end
+[frequency, child_count] = hist(offspring_counts, max(offspring_counts));
+child_count = child_count - adj;
+child_count = child_count(frequency > 0);
+frequency = frequency(frequency > 0);
+
+for index = 1 : length(unique_ids_last_10p)
+    id = unique_ids_last_10p(index);
+    offspring_counts_last_10p(index) = sum(id == data_last_10p);
+end
+[frequency_last_10p, child_count_last_10p] = hist(offspring_counts_last_10p, max(offspring_counts_last_10p));
+child_count_last_10p = child_count_last_10p - adj;
+child_count_last_10p = child_count_last_10p(frequency_last_10p > 0);
+frequency_last_10p = frequency_last_10p(frequency_last_10p > 0);
 
 figure;
 hold all;
 
 % Data
 plot(child_count, frequency, '.', 'LineWidth', 3, 'DisplayName', 'Data');
+plot(child_count_last_10p, frequency_last_10p, '.', 'LineWidth', 3, 'DisplayName', 'Data (last 10%)');
 
 % Null-hypothesis fit
 A = sum(child_count.*frequency)/2;
+A_last_10p = sum(child_count_last_10p.*frequency_last_10p)/2;
 x_fit = linspace(min(child_count), max(child_count), 1000);
+x_fit_last_10p = linspace(min(child_count_last_10p), max(child_count_last_10p), 1000);
 A_text='';
 if min(x_fit) == 0
     A_text = '1/2 ';
 end
 y_fit = A*(1/2).^(x_fit);
+y_fit_last_10p = A_last_10p*(1/2).^(x_fit_last_10p);
 plot(x_fit, y_fit, 'LineWidth', 3, 'DisplayName', 'Null hypothesis');
+plot(x_fit_last_10p, y_fit_last_10p, 'LineWidth', 3, 'DisplayName', 'Null hypothesis (last 10%)');
 
 % Error bars
 high_err = sqrt(frequency);
+high_err_last_10p = sqrt(frequency_last_10p);
 low_err = min(high_err, frequency - min(y_fit));
+low_err_last_10p = min(high_err_last_10p, frequency_last_10p - min(y_fit_last_10p));
 errorbar(child_count, frequency, low_err, high_err, '.');
+errorbar(child_count_last_10p, frequency_last_10p, low_err_last_10p, high_err_last_10p, '.');
 
 % Info box
 fit_text = {'y = A(1/2)^{x}',
@@ -55,8 +89,8 @@ yl = ylim;
 fit_box = text(0.5*xl(2), 0.4*yl(2), fit_text);
 
 leg = legend('show');
-xlabel(data.colheaders(1));
-ylabel(data.colheaders(2));
+xlabel('Number of offspring');
+ylabel('Frequency');
 title('Offspring Count');
 print([raw_data_file_name '_offspring_plot_lin.png']);
 
