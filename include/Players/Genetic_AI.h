d9412a5248e4cc8970bd97a0b217c733b6bf67a0
Mark Harrison <mark.z.harrison@gmail.com>
2020-02-25 02:55:13 -0800

    Explain how Genetic_AIs are sorted

--- include/Players/Genetic_AI.h
+++ include/Players/Genetic_AI.h
@@ -84,7 +84,7 @@ class Genetic_AI : public Minimax_AI
         //! \returns The ID number.
         int id() const noexcept;
 
-        //! \brief A comparison function to sort Genetic_AI collections.
+        //! \brief A comparison function to sort Genetic_AI collections by ID number.
         //!
         //! \param other Another Genetic_AI.
         //! \returns If the other AI should go after this AI.
