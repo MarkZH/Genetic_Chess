f77c02d207fb9a7a0e040225829dde04c4a80c85
Mark Harrison <mark.z.harrison@gmail.com>
2020-03-18 18:40:15 -0700

    Name all arguments to shell script

--- analysis/offspring_frequency.sh
+++ analysis/offspring_frequency.sh
@@ -4,8 +4,9 @@
 # played by an AI equals the number of its offspring.
 
 output_file="${1}_offspring_counts.txt"
+reproduction_type="$2"
 grep "Genetic" "$1" |  # Find players
     cut -d\" -f2 |     # Cut out name of player
     cut -d' ' -f3 > "$output_file" # Cut out ID number
 
-octave ./analysis/offspring_plots.m "$output_file" "$2"
+octave ./analysis/offspring_plots.m "$output_file" "$reproduction_type"
