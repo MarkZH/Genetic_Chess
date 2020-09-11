01269ecdd43e5d577d6b25ca01a06d1796b4cc29
Mark Harrison <mark.z.harrison@gmail.com>
2020-09-11 00:49:01 -0700

    Mark gene_pool() as [[noreturn]]
    This function is only stopped by the user pressing Ctrl-C.

--- include/Genes/Gene_Pool.h
+++ include/Genes/Gene_Pool.h
@@ -8,6 +8,6 @@
 //! \brief Run a gene pool according to a configuration text file.
 //!
 //! \param config_file The name of the file with the gene pool configuration data.
-void gene_pool(const std::string& config_file);
+[[noreturn]] void gene_pool(const std::string& config_file);
 
 #endif // GENE_POOL_H
