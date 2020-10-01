a1d666149e4e70fe13db4fb432bd0b83aa2e3f6d
Mark Harrison <mark.z.harrison@gmail.com>
2020-09-30 17:20:55 -0700

    Delete Scoped Stopwatch

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,6 @@
 
 #include "Utility/Exceptions.h"
 #include "Utility/String.h"
-#include "Utility/Scoped_Stopwatch.h"
 
 #include "Testing.h"
 
@@ -51,8 +50,6 @@ namespace
 //! \returns EXIT_SUCCESS or EXIT_FAILURE.
 int main(int argc, char *argv[])
 {
-    auto printer = Scoped_Stopwatch::print_guard();
-
     try
     {
         if(argc > 1)
