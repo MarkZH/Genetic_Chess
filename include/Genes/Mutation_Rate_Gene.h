a57b1c65f10886d5e83ad89001df0de39bdc2cd0
Mark Harrison <mark.z.harrison@gmail.com>
2020-11-18 21:10:31 -0800

    Private methods should stay private

--- include/Genes/Mutation_Rate_Gene.h
+++ include/Genes/Mutation_Rate_Gene.h
@@ -19,14 +19,13 @@ class Mutation_Rate_Gene : public Clonable_Gene<Mutation_Rate_Gene>
         //! \returns An integer number that determines the number of point mutations the Genome::mutate() makes.
         int mutation_count() const noexcept;
 
-        void gene_specific_mutation() noexcept override;
-
     private:
         double mutated_components_per_mutation = 3.0;
 
         double score_board(const Board& board, Piece_Color perspective, size_t depth) const noexcept override;
         void adjust_properties(std::map<std::string, double>& properties) const noexcept override;
         void load_gene_properties(const std::map<std::string, double>& properties) override;
+        void gene_specific_mutation() noexcept override;
 };
 
 #endif // MUTATION_RATE_GENE_H
