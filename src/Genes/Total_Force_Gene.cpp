4bb33138f737427ffde8988910e14453fba6401d
Mark Harrison <mark.z.harrison@gmail.com>
2021-03-07 19:36:08 -0800

    Small speedup for Total Force Gene

--- src/Genes/Total_Force_Gene.cpp
+++ src/Genes/Total_Force_Gene.cpp
@@ -15,13 +15,14 @@ Total_Force_Gene::Total_Force_Gene(const Piece_Strength_Gene* piece_strength_sou
 
 double Total_Force_Gene::score_board(const Board& board, Piece_Color perspective, size_t, double) const noexcept
 {
+    auto values = piece_strength_source->piece_values();
     return std::accumulate(Square::all_squares().begin(), Square::all_squares().end(), 0.0,
-                           [this, &board, perspective](auto sum, auto square)
+                           [&values, &board, perspective](auto sum, auto square)
                            {
                                auto piece = board.piece_on_square(square);
                                if(piece && piece.color() == perspective)
                                {
-                                   return sum + piece_strength_source->piece_value(piece);
+                                   return sum + values[static_cast<size_t>(piece.type())];
                                }
                                else
                                {
