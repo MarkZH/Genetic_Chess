c4301b1a1d153858b1de7dbc8ff188ff39366a31
Mark Harrison <mark.z.harrison@gmail.com>
2021-03-12 23:36:06 -0800

    constexpr does imply const

--- include/Players/Game_Tree_Node_Result.h
+++ include/Players/Game_Tree_Node_Result.h
@@ -25,9 +25,9 @@ struct Game_Tree_Node_Result
     std::vector<const Move*> variation;
 
     //! \brief The score assigned to a checkmate board position for the winning Minimax_AI.
-    static constexpr const auto win_score = std::numeric_limits<double>::infinity();
+    static constexpr auto win_score = std::numeric_limits<double>::infinity();
     //! \brief The score assigned to a checkmate board position for the losing Minimax_AI.
-    static constexpr const auto lose_score = -win_score;
+    static constexpr auto lose_score = -win_score;
 
     //! Indicates the result is a draw, which requires some special handling.
     bool is_draw;
