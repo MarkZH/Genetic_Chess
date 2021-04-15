5c4ac307ba86422595c4d1c38e35aa0f60690919
Mark Harrison <mark.z.harrison@gmail.com>
2021-04-15 12:26:35 -0700

    Count total castle for each side
    Also, break up long line.

--- analysis/castling.sh
+++ analysis/castling.sh
@@ -8,7 +8,15 @@ count_castles()
     # $2 = Move separator after filtering extra spaces
     # $3 = Which move to use (2 = White, 3 = Black, 1 = Both after separating moves by newline)
     delete_comments="./$(dirname "$0")/delete_comments.sh"
-    grep O "$1" | "$delete_comments" | tr -s ' ' "$2" | cut -d' ' -f"$3" | grep O | tr -d +\# | sort | uniq -c
+    grep O "$1" \
+		| "$delete_comments" \
+		| tr -s ' ' "$2" \
+		| cut -d' ' -f"$3" \
+		| grep O \
+		| tr -d +\# \
+		| sed 's/.*/\0\nTotal/' \
+		| sort \
+		| uniq -c
 }
 
 echo "# Castling"
