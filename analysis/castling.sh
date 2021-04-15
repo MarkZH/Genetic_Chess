f7e1d5ce707624af493ffb57c3833d604c586649
Mark Harrison <mark.z.harrison@gmail.com>
2021-04-14 18:49:09 -0700

    Improve castling stats script
    Separate castling counts by color.

--- analysis/castling.sh
+++ analysis/castling.sh
@@ -2,6 +2,22 @@
 
 # Count how many times a castling move was picked (O-O = kingside, O-O-O = queenside)
 
-bindir="$(dirname "$0")"
-(echo "# Castling" ; grep O "$1" | ./"$bindir"/delete_comments.sh | tr -s '[:space:]' '[\n*]' | grep O | tr -d +\#  | sort | uniq -c)
+count_castles()
+{
+    # $1 = PGN file name
+    # $2 = Move separator after filtering extra spaces
+    # $3 = Which move to use (2 = White, 3 = Black, 1 = Both after separating moves by newline)
+    delete_comments="./$(dirname "$0")/delete_comments.sh"
+    grep O "$1" | "$delete_comments" | tr -s ' ' "$2" | cut -d' ' -f"$3" | grep O | tr -d +\# | sort | uniq -c
+}
+
+echo "# Castling"
+count_castles "$1" '\n' 1
+
+echo "# White Castling"
+count_castles "$1" ' ' 2
+
+echo "# Black Castling"
+count_castles "$1" ' ' 3
+
 echo "out of $(grep -c Round "$1") games"
