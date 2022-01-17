fd1f5bb3bf922fcfd5afdb5d6a0faced6eb995b3
Mark Harrison <mark.z.harrison@gmail.com>
2022-01-16 21:46:27 -0800

    Fix public/private members and doxygen comments

--- include/Genes/Pawn_Structure_Gene.h
+++ include/Genes/Pawn_Structure_Gene.h
@@ -10,12 +10,12 @@
 
 class Board;
 
+//! \brief A gene to evaluate how well pawns are protected.
 class Pawn_Structure_Gene : public Clonable_Gene<Pawn_Structure_Gene>
 {
     public:
         Pawn_Structure_Gene() noexcept;
         std::string name() const noexcept override;
-        double score_board(const Board& board, Piece_Color perspective, size_t depth, double game_progress) const noexcept override;
 
     private:
         double opening_guarded_by_pawn = 1.0;
@@ -26,6 +26,7 @@ class Pawn_Structure_Gene : public Clonable_Gene<Pawn_Structure_Gene>
         double endgame_guarded_by_pawn_in_one_move = 1.0;
         double endgame_guarded_by_piece = 1.0;
 
+        double score_board(const Board& board, Piece_Color perspective, size_t depth, double game_progress) const noexcept override;
         void gene_specific_mutation() noexcept override;
         void adjust_properties(std::map<std::string, double>& properties) const noexcept override;
         void load_gene_properties(const std::map<std::string, double>& properties) override;
