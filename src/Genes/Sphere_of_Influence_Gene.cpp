6886f1638e66f062f67df59e87120c4fc9d3a23b
Mark Harrison <mark.z.harrison@gmail.com>
2022-04-22 22:12:03 -0700

    Change operation of Sphere of Influence Gene
    Instead of scoring how close an attacked square is to the king, score
    based on how far the square is into the opponent's territory. Remove the
    mutable aspect of this scoring.
    
    Alter tests and test data as needed.

--- src/Genes/Sphere_of_Influence_Gene.cpp
+++ src/Genes/Sphere_of_Influence_Gene.cpp
@@ -12,27 +12,6 @@
 #include "Utility/Random.h"
 #include "Utility/Math.h"
 
-namespace
-{
-    const auto inv_king_dist_plus_one =
-        []()
-        {
-            std::array<std::array<double, 64>, 64> result;
-            for(const auto square_a : Square::all_squares())
-            {
-                for(const auto square_b : Square::all_squares())
-                {
-                    // The "king distance" between two squares is the minimum number
-                    // of moves a king needs to get from one square to the other.
-                    const auto diff = square_a - square_b;
-                    result[square_a.index()][square_b.index()] =
-                        1.0/(1.0 + std::max(std::abs(diff.file_change), std::abs(diff.rank_change)));
-                }
-            }
-            return result;
-        }();
-}
-
 Sphere_of_Influence_Gene::Sphere_of_Influence_Gene() noexcept
 {
     normalize_square_scores();
@@ -42,14 +21,12 @@ void Sphere_of_Influence_Gene::adjust_properties(std::map<std::string, std::stri
 {
     legal_square_scores.write_to_map(properties);
     illegal_square_scores.write_to_map(properties);
-    king_target_factors.write_to_map(properties);
 }
 
 void Sphere_of_Influence_Gene::load_gene_properties(const std::map<std::string, std::string>& properties)
 {
     legal_square_scores.load_from_map(properties);
     illegal_square_scores.load_from_map(properties);
-    king_target_factors.load_from_map(properties);
     normalize_square_scores();
 }
 
@@ -62,9 +39,6 @@ double Sphere_of_Influence_Gene::score_board(const Board& board, Piece_Color per
 {
     const auto legal_square_score = legal_square_scores.interpolate(game_progress);
     const auto illegal_square_score = illegal_square_scores.interpolate(game_progress);
-    const auto king_target_factor = king_target_factors.interpolate(game_progress);
-    const auto opponent_king_square = board.find_king(opposite(perspective));
-    const auto& inv_dist_plus_one = inv_king_dist_plus_one[opponent_king_square.index()];
 
     double score = 0.0;
     for(const auto square : Square::all_squares())
@@ -83,29 +57,23 @@ double Sphere_of_Influence_Gene::score_board(const Board& board, Piece_Color per
             continue;
         }
 
-        const auto inv_dist_to_king_plus_one = inv_dist_plus_one[square.index()];
-        score += square_score*(1 + king_target_factor*inv_dist_to_king_plus_one);
+        const auto invasion_distance = (perspective == Piece_Color::WHITE ? square.rank() : 9 - square.rank());
+        score += square_score*invasion_distance;
     }
 
-    return score/64;
+    return score/(64*8);
 }
 
 
 void Sphere_of_Influence_Gene::gene_specific_mutation() noexcept
 {
-    switch(Random::random_integer(1, 3))
+    if(Random::coin_flip())
     {
-        case 1:
-            legal_square_scores.mutate();
-            break;
-        case 2:
-            king_target_factors.mutate();
-            break;
-        case 3:
-            illegal_square_scores.mutate();
-            break;
-        default:
-            assert(false);
+        legal_square_scores.mutate();
+    }
+    else
+    {
+        illegal_square_scores.mutate();
     }
 
     normalize_square_scores();
