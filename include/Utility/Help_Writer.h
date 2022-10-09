664e99d1ca37d703f34fb9fc3b33aa7b67ebde07
Mark Harrison <mark.z.harrison@gmail.com>
2022-10-08 17:07:56 -0700

    Document Help_Writer::operator<<()

--- include/Utility/Help_Writer.h
+++ include/Utility/Help_Writer.h
@@ -44,6 +44,7 @@ class Help_Writer
         void add_option(const std::string& name,
                         const std::string& description) noexcept;
 
+        //! \brief Output stream operator that prints the full text of the help file.
         friend std::ostream& operator<<(std::ostream& os, const Help_Writer& help) noexcept;
 
     private:
