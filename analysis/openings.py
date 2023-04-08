0110f46e8903f84df243b57100aa912e5f878fdf
Mark Harrison <mark.z.harrison@gmail.com>
2023-04-08 15:15:44 -0700

    Simplify opening move parsing

--- analysis/openings.py
+++ analysis/openings.py
@@ -6,14 +6,6 @@ from delete_comments import delete_comments
 
 # Determine most often used openings
 
-def remove_text(line: str, excision: str) -> str:
-    index = line.find(excision)
-    if index != -1:
-        line = line[:index] + line[index + len(excision):]
-        return remove_text(line, excision)
-    else:
-        return ' '.join(line.split())
-
 def get_opening_files(game_file: str) -> List[str]:
     outfile = f"{game_file}_opening_list.txt"
     white_file = outfile + "_white.txt"
@@ -25,17 +17,21 @@ def get_opening_files(game_file: str) -> List[str]:
                 continue
 
             line = delete_comments(line)
-            for ending in ['1-0', '0-1', '1/2-1/2']:
-                line = remove_text(line, ending)
-            try:
-                _, white_move, black_move = line.split()
-            except ValueError:
-                continue
-            opening = f"{white_move} {black_move}"
-            output.write(f"{opening}\n")
-            white_output.write(f"{white_move}\n")
-            black_output.write(f"{black_move}\n")
-            unique_opening_counter[opening] += 1
+            parts = line.split()
+
+            def get_part(index):
+                return parts[index] if len(parts) > index and not parts[index][0].isdigit() else ""
+            
+            white_move = get_part(1)
+            black_move = get_part(2)
+            opening = f"{white_move} {black_move}".strip()
+            if opening:
+                output.write(f"{opening}\n")
+                unique_opening_counter[opening] += 1
+            if white_move:
+                white_output.write(f"{white_move}\n")
+            if black_move:
+                black_output.write(f"{black_move}\n")
 
     count_column_width = len(str(max(unique_opening_counter.values())))
     totals = [(count, opening) for opening, count in unique_opening_counter.items()]
