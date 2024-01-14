70fb2927ea920d6c7a9755a021a495088e1230a7
Mark Harrison <mark.z.harrison@gmail.com>
2024-01-14 03:40:36 -0800

    Turn promotion-counting loop into method call

--- analysis/promotions.py
+++ analysis/promotions.py
@@ -9,9 +9,6 @@ def count_promotions(all_games: list[Game_Record]) -> None:
     print("\n# Promotions")
     promotion_counts: Counter = Counter()
     for game in all_games:
-        for move in game.moves:
-            if "=" in move:
-                promotion = move.split("=")[1][0]
-                promotion_counts[promotion] += 1
+        promotion_counts.update(move[move.index("=") + 1] for move in game.moves if "=" in move)
 
     print_sorted_count_table(promotion_counts.items())
