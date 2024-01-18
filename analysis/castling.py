846190f8bd05a2f6520347506c8a3d24cacdfc4b
Mark Harrison <mark.z.harrison@gmail.com>
2024-01-17 23:22:21 -0800

    Add plot for castles per game

--- analysis/castling.py
+++ analysis/castling.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from enum import StrEnum, auto
 import numpy as np
 import matplotlib.pyplot as plt
-from common import read_all_games, print_sorted_count_table, picture_file_args
+from common import read_all_games, print_sorted_count_table, picture_file_args, moving_mean, centered_x_axis
 
 
 def delete_checkmarks(move: str) -> str:
@@ -31,21 +31,36 @@ def count_all_castles(games_file_name: str) -> None:
                 castle_count[color][delete_checkmarks(moves[castle_index])] += 1
             games_where_castled[color].append(castled)
 
-    fig, ax = plt.subplots()
+    cumulative_figure, cumulative_axes = plt.subplots()
+    rate_figure, rate_axes = plt.subplots()
+    for color in Color:
+        castling_games = np.array(games_where_castled[color], dtype=int)
+        cumulative_castles = np.cumsum(castling_games)
+        x_axis = np.arange(cumulative_castles.size) + 1
+        cumulative_axes.plot(x_axis, cumulative_castles, label=color.title())
+
+        castling_rate = 100*moving_mean(castling_games, 10000)
+        rate_x_axis = centered_x_axis(x_axis, castling_rate)
+        rate_axes.plot(rate_x_axis, castling_rate, label=color.title())
+
+    cumulative_axes.set_title("Total number of castles")
+    cumulative_axes.set_ylabel("Cumulative castle count")
+    cumulative_axes.set_xlabel("Game count")
+    cumulative_axes.legend()
+    cumulative_figure.tight_layout()
+    cumulative_figure.savefig(f"{games_file_name}_castle_counts.{picture_file_args['format']}", **picture_file_args)
+    plt.close(cumulative_figure)
+
+    rate_axes.set_title("Percent of games with castling")
+    rate_axes.set_ylabel("Castling percentage")
+    rate_axes.set_xlabel("Game count")
+    rate_axes.legend()
+    rate_figure.tight_layout()
+    rate_figure.savefig(f"{games_file_name}_castle_rate.{picture_file_args['format']}", **picture_file_args)
+    plt.close(rate_figure)
+
     for color in Color:
         castle_count[color]["Total"] = sum(castle_count[color].values())
         print("\n" + f"# {color.title()}".strip() + " castling")
         print_sorted_count_table(castle_count[color].items())
         print(f"\nout of {len(games_where_castled[color])} games")
-        cumulative_castles = np.cumsum(np.array(games_where_castled[color], dtype=int))
-        x_axis = np.arange(cumulative_castles.size) + 1
-        ax.plot(x_axis, cumulative_castles, label=color.title())
-
-    ax.set_title("Total number of castles")
-    ax.set_ylabel("Cumulative castle count")
-    ax.set_xlabel("Game count")
-    ax.legend()
-
-    fig.tight_layout()
-    fig.savefig(f"{games_file_name}_castle_counts.{picture_file_args['format']}", **picture_file_args)
-    plt.close(fig)
