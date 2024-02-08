ea536531acb0f3fbc0ddf5f33b33ca4b5c15df8b
Mark Harrison <mark.z.harrison@gmail.com>
2024-02-07 23:09:57 -0800

    Allow activation points to be reversed ordered
    Similarly to allowing the values to range from -0.05 or 1.05,
    this will allow mutations to deactivate a gene with some
    margin for error so that genes don't reactivate for one move.

--- src/Genes/Gene.cpp
+++ src/Genes/Gene.cpp
@@ -209,8 +209,10 @@ void Gene::mutate() noexcept
         const auto upper_limit = 1.0 + end_margin;
         mutating_value.value() = std::clamp(mutating_value.value(), lower_limit, upper_limit);
         
-        const auto clamping_lower_limit = mutate_turn_on ? mutating_value.value() : lower_limit;
-        const auto clampling_uper_limit = mutate_turn_on ? upper_limit : mutating_value.value();
+        // Allow the begin and end of activation to be in reverse order by the same margin to
+        // more firmly shut off the gene.
+        const auto clamping_lower_limit = mutate_turn_on ? mutating_value.value() - end_margin : lower_limit;
+        const auto clampling_uper_limit = mutate_turn_on ? upper_limit : mutating_value.value() + end_margin;
         clamping_value.value() = std::clamp(clamping_value.value(), clamping_lower_limit, clampling_uper_limit);
     }
     else
