eb7b2444e7cd9b3e2bd8241245864566f988b3fc
Mark Harrison <mark.z.harrison@gmail.com>
2024-02-07 23:20:15 -0800

    Plot when genes are inactive

--- analysis/gene_plots.py
+++ analysis/gene_plots.py
@@ -162,6 +162,10 @@ def plot_genome(gene_pool_filename: str) -> None:
                     these_axes.plot(common.centered_x_axis(id_list, activation_begin_data), activation_begin_data, style, markersize=markersize, linewidth=linewidth, label="Begin")
                 these_axes.plot(common.centered_x_axis(id_list, d), d, style, markersize=markersize, linewidth=linewidth, label=label)
                 if is_activation_end:
+                    gene_is_active = this_data <= activation_begin_data
+                    if np.any(gene_is_active):
+                        mean_activation_point = (this_data + activation_begin_data)/2
+                        these_axes.plot(id_list[gene_is_active], mean_activation_point[gene_is_active], style, markersize=markersize, linewidth=linewidth, label="Inactive")
                     these_axes.axhline(y=1.0, color=common.plot_params["x-axis color"], linewidth=common.plot_params["x-axis weight"])
                     legend = these_axes.legend()
                     for line in legend.get_lines():
