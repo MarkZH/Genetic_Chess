405b8f4f5b819b01703b8ee6401dde68d403e509
Mark Harrison <mark.z.harrison@gmail.com>
2024-07-04 02:25:09 -0700

    Move const variables out of loop

--- src/Utility/String.cpp
+++ src/Utility/String.cpp
@@ -144,11 +144,12 @@ std::string String::date_and_time_format(const std::chrono::system_clock::time_p
 
 std::string String::word_wrap(const std::string& text, const size_t line_length, const size_t indent) noexcept
 {
+    const std::string space = " ";
+    const auto indent_space = std::string(indent, ' ');
+
     std::vector<std::string> lines;
     for(const auto& word : split(text))
     {
-        const auto indent_space = std::string(indent, ' ');
-        const std::string space = " ";
         if(lines.empty() || lines.back().size() + space.size() + word.size() > line_length)
         {
             lines.push_back(indent_space + word);
